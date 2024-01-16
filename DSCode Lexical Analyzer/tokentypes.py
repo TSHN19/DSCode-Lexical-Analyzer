79d9b1106d53aee1aebbacbaa672fad0cc203922
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-01-17 00:53:16 +0800

    Update tokentypes.py

--- DSCode Lexical Analyzer/tokentypes.py
+++ DSCode Lexical Analyzer/tokentypes.py
@@ -1,3 +1,8 @@
+alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
 # KEYWORDS
 TT_ControlFlowKeywords = {
     "break", 
@@ -106,7 +111,6 @@ TT_Operators = [
     ('[]', "Brackets"),
     ('()', "Parenthesis"),
     ('{}', "Braces"),
-    ('//', "Comment"),
     ('/*', "Multiple Line Comment Opening"),
     ('*/', "Multiple Line Comment Closing")
 ]
