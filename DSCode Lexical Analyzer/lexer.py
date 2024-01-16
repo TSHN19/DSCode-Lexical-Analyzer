9960496e2ff3ae421e5d522fd8ced507501b177b
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-01-16 23:22:52 +0800

    Update lexer.py

--- DSCode Lexical Analyzer/lexer.py
+++ DSCode Lexical Analyzer/lexer.py
@@ -1,9 +1,9 @@
-from tokentypes import TT_SpecialSymbols, TT_Operators, TT_ControlFlowKeywords, TT_DataTypeKeywords, TT_StorageClassKeywords, TT_OtherKeywords
+from tokentypes import TT_Operators, TT_ControlFlowKeywords, TT_DataTypeKeywords, TT_StorageClassKeywords, TT_OtherKeywords
     
 def lexical_analyzer(code):
     lexemes = []
     lexemes_display = []
-    tokens = []
+    tokens_display = []
     current_token = ''
     invalid_token = ''
     string_delimiter_count = 0
@@ -11,12 +11,12 @@ def lexical_analyzer(code):
     double_operator = False
     triple_operator = False
 
-    operatorSymbols = [symbol for symbol, _ in TT_Operators]
-    specialSymbols = [symbol for symbol, _ in TT_SpecialSymbols]
+    symbols = [symbol for symbol, _ in TT_Operators]    
     controlflow = list(TT_ControlFlowKeywords)
     datatype = list(TT_DataTypeKeywords)
     storageclass = list(TT_StorageClassKeywords)
     otherkeywords = list(TT_OtherKeywords)
+
     
     # Iterate through each character in the code
     for i in range(len(code)):
@@ -26,13 +26,6 @@ def lexical_analyzer(code):
         if ((char == ' ') or (char == '\n') or (char == '\t')) and in_string is False:
             continue
 
-        # Check for invalid tokens
-         # This part of the lexical for digit when identifying an identifier with number as the start
-         # This is to skip the remaining char for an identifier with number as the start
-        elif char in invalid_token:
-            invalid_token = invalid_token[1:]
-            continue
-        
         # Check if the character is a string delimiter
         elif (char == '"') or (char == "'"):
             # Toggle string mode 
@@ -40,51 +33,70 @@ def lexical_analyzer(code):
             string_delimiter_count += 1
             current_token += char
 
+            # Set as invalid, unless encounters a closing delimiter
             if string_delimiter_count == 1:
-                tokens.append("ERROR: Incomplete String Closing Delimiter(" + char + ")")
+                tokens_display.append("ERROR: Incomplete String Closing Delimiter(" + char + ")")
                 string_char = char
                 lexemes.append("ERROR: Invalid Token")
-                lexemes_display.append(string_char + '...')
+                lexemes_display.append(string_char)
 
+            # If encounters a closing delimiter, close the string input
             elif string_delimiter_count == 2:
                 lexemes[-1] = current_token
-                string_display = current_token[1 : -1]
-                lexemes_display[-1] = string_char + string_display[:2] + '...' + char
                 string_delimiter_count = 0
-                current_token = ''
 
-                if string_char == char:
-                    tokens[-1] = "String"
+                # Check if string is a string literal
+                if (string_char == char) and (string_char == '"'):
+                    string_display = current_token[1 : -1]
+                    lexemes_display[-1] = string_char + string_display[:9] + '...' + char
+                    tokens_display[-1] = "String Literal"
+
+                # Check if string uses single quotes delimiter
+                elif (string_char == char) and (string_char == "'"):
+                    # If the input is a single character
+                    if len(current_token) == 3:
+                        lexemes_display[-1] = current_token
+                        tokens_display[-1] = "Character Literal"
+                    
+                    # Else, notify wrong delimiter used
+                    else:
+                        tokens_display[-1] = "ERROR: Character Literal Delimiter Used for String Literal"
+
+                # Notify different string delimiters employed
                 else:
-                    tokens[-1] = "ERROR: Different String Delimiters Employed"
+                    tokens_display[-1] = "ERROR: Different String Delimiters Employed"
+                
+                current_token = ''
         
+        # While in_string is true, all following inputs will count as String Literal
         elif in_string:
             # Inside a string literal
             current_token += char
+
+            if len(current_token) < 14:
+                lexemes_display[-1] = current_token[:10] + "...?"
+
         
-
-        # ----Fix three operators, separate 
-        elif char in operatorSymbols:
-
-            # Check if character is part of a three symbol operator
-            if (len(code) > i + 2) and ((char + code[i + 1] + code[i + 2]) in operatorSymbols):
-                triple_operator = not triple_operator
+        # Check if input is a operator or special symbol
+        elif (char in symbols):
+            
+            if (len(code) > i + 2) and (char + code[i + 1] + code[i + 2]) in (symbols) and (triple_operator is False):
                 # Build token
                 current_token += char
+                triple_operator = not triple_operator
                 continue
 
             elif triple_operator:
-                # Build token
-                current_token += char
-                
-                if len(current_token) == 3:
-                    print(current_token)
-                    # index = operatorSymbols.index(current_token)
-                    lexemes.append(current_token)
-                    lexemes_display.append(current_token)
-                    triple_operator = not triple_operator
+                if (len(current_token) < 2):
+                    current_token += char
+                    continue
 
-            elif (char + code[i + 1]) in operatorSymbols and (double_operator is False):
+                current_token += char
+                lexemes.append(current_token)
+                lexemes_display.append(current_token)
+                triple_operator = not triple_operator
+            
+            elif (char + code[i + 1]) in symbols and (double_operator is False) and (triple_operator is False):
                 # Build token
                 current_token += char
                 double_operator = not double_operator
@@ -92,53 +104,37 @@ def lexical_analyzer(code):
             
             elif double_operator:
                 # Get the index of char in the double operators list
-                index = operatorSymbols.index(current_token + char)
-                lexemes.append(current_token + char)
-                lexemes_display.append(current_token + char)
-                double_operator = not double_operator
-
-            else:
-                # Get the index of char in the single operators list
-                index = operatorSymbols.index(char)
-                lexemes.append(char)
-                lexemes_display.append(char)
-
-            # Use the index to access the corresponding token description
-            token_description = TT_Operators[index][1]
-            tokens.append(token_description + " Operator")
-            current_token = ''
-        
-        elif char in specialSymbols:
-            if char =='.' and current_token.isdigit():
                 current_token += char
-                continue
-
-            elif (char + code[i + 1]) in specialSymbols and (double_operator is False):
-                # Build token
-                current_token += char
+                lexemes.append(current_token)
+                lexemes_display.append(current_token)
                 double_operator = not double_operator
-                continue
             
-            elif double_operator:
-                # Get the index of char in the special symbols list
-                index = specialSymbols.index(current_token + char)
-                lexemes.append(current_token + char)
-                lexemes_display.append(current_token + char)
-                double_operator = not double_operator
-
             else:
                 # Get the index of char in the single operators list
-                index = specialSymbols.index(char)
+                current_token += char
                 lexemes.append(char)
                 lexemes_display.append(char)
 
-            # Use the index to access the corresponding token description
-            token_description = TT_SpecialSymbols[index][1]
-            tokens.append(token_description)
-            current_token = ''
+            # Implement exception handling for when ValueError is encountered
+            try:
+                # Use the index to access the corresponding token description
+                index = symbols.index(current_token)
+                token_description = TT_Operators[index][1]
+                tokens_display.append(token_description)
+                current_token = ''
+
+            except ValueError:
+                tokens_display.append("ERROR: Invalid Token")
+
+        # Check for invalid tokens
+        # This part of the lexical for digit when identifying an identifier with number as the start
+        # This is to skip the remaining char for an identifier with number as the start
+        elif char in invalid_token:
+            invalid_token = invalid_token[1:]
+            continue
 
         #KEYWORDS AND IDENTIFIER
-            
+
         # Checks if the char is alphabet, _, or a digit 
         # accepts the digit only if the current token is not empty meaning there is a word before the number
         # and when the current token is not all digits(need kasi na pag digits sa number sha)
@@ -146,40 +142,30 @@ def lexical_analyzer(code):
             # The character is alphabetical, _, add it to the current token
             current_token += char
             if (i + 1 < len(code)) and (not code[i + 1].isalnum() and code[i + 1] != '_'):
-                # Check if the current token is a data type keyword
+                # Check if the current token is a data type keywordnot(code[i + 1].isalpha()):
+                lexemes.append(current_token)
+                lexemes_display.append(current_token)
+
                 if current_token in datatype:
-                    lexemes.append(current_token)
-                    lexemes_display.append(current_token)
-                    tokens.append("Data Type")
-                    current_token = ''
+                    tokens_display.append("Data Type Keyword")
 
                 elif current_token in controlflow:
-                    lexemes.append(current_token)
-                    lexemes_display.append(current_token)
-                    tokens.append("Control Flow")
-                    current_token = ''
-                    
+                    tokens_display.append("Control Flow Keyword")
+
                 elif current_token in storageclass:
-                    lexemes.append(current_token)
-                    lexemes_display.append(current_token)
-                    tokens.append("Storage Class")
-                    current_token = ''
+                    tokens_display.append("Storage Class Keyword")
 
                 elif current_token in otherkeywords:
-                    lexemes.append(current_token)
-                    lexemes_display.append(current_token)
-                    tokens.append("Other Keywords")
-                    current_token = ''
+                    tokens_display.append("Other Keywords")
 
                 else:
-                    # The current token is not a data type keyword, treat as an identifier
-                    lexemes.append(current_token)
-                    lexemes_display.append(current_token)
-                    tokens.append("Identifier")
-                    current_token = ''
+                    tokens_display.append("Identifier")
+                
+                current_token = ''
+
             else: continue
         
-        #If char is a number
+        # Check if input is a numeric constant
         elif char.isdigit() or char == '.':
             current_token += char
             # This is to check whether the digit is followed by a alphabet or _ (since bawal nga sha sa rule ng identifier)
@@ -194,7 +180,7 @@ def lexical_analyzer(code):
                     j+=1
                 lexemes.append(current_token)
                 lexemes_display.append(current_token)
-                tokens.append("ERROR: Invalid Token")
+                tokens_display.append("ERROR: Invalid Token")
 
                 # eto yung invalid token sa taas, pinasa sha para lagpasan na lang yung part pa nung identifier
                 invalid_token = current_token
@@ -204,24 +190,20 @@ def lexical_analyzer(code):
             elif (i + 1 < len(code)) and (not code[i + 1].isdigit() and '.' in current_token):
                 lexemes.append(current_token)
                 lexemes_display.append(current_token)
-                tokens.append("Float")
+                tokens_display.append("Float")
                 current_token = ''
 
             #pa add na lang ako here paano yung sa float
             elif (i + 1 < len(code)) and (not code[i + 1].isdigit() and code[i + 1] != '.'):
                 lexemes.append(current_token)
                 lexemes_display.append(current_token)
-                tokens.append("Digit")
+                tokens_display.append("Digit")
                 current_token = ''
-            
-
-            else:
-                continue
 
         else:
             lexemes.append(char)
             lexemes_display.append(char)
-            tokens.append("ERROR: Invalid Token")
+            tokens_display.append("ERROR: Invalid Token")
             current_token = ''
     
-    return lexemes_display, tokens
+    return lexemes_display, tokens_display
