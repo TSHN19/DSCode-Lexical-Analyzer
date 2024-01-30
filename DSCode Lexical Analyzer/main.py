faed066703a4296466131ac593a4f311eb778a7c
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-01-30 14:39:16 +0800

    Update main.py

--- DSCode Lexical Analyzer/main.py
+++ DSCode Lexical Analyzer/main.py
@@ -1,4 +1,5 @@
-from lexer import lexical_analyzer
+from dsc_lexer import lexical_analyzer
+from dsc_parser import syntax_analyzer
 
 # GUI Library Import
 from tkinter.filedialog import askopenfilename, asksaveasfile, asksaveasfilename
@@ -36,7 +37,7 @@ class GUI:
         self.left_frame = Frame(
             master = self.root, 
             width = 525, 
-            height = 540, 
+            height = 390, 
             bg = "#E2E2E2", 
             highlightthickness = 2.3, 
             highlightbackground = "black")
@@ -54,16 +55,32 @@ class GUI:
         self.right_frame.place(x = 595, y = 90)
         self.right_frame.pack_propagate(False)
 
+        # Third Frame: Syntax Analysis
+        self.third_frame = Frame(
+            master = self.root, 
+            width = 525, 
+            height = 135, 
+            bg = "#E2E2E2", 
+            highlightthickness = 2.3, 
+            highlightbackground = "black")
+        self.third_frame.place(x = 35, y = 495)
+        self.third_frame.pack_propagate(False)
+
+        # Create a text widget for syntax errors
+        self.syntax_errors = Text(self.third_frame, bg = "#E2E2E2", borderwidth = 0, padx = 5, pady = 5)
+        self.syntax_errors.place(relx = 0.01, y = 18, relwidth = 0.98, height = 108)
+        self.syntax_errors.config(state = "disabled", wrap = tk.WORD, font = ("Courier", 10), spacing1 = 4)
+
         # Create a text widget for code input number line
         self.number_line_left = Text(self.left_frame, bg = "#E2E2E2", borderwidth = 0, padx = 5, pady = 5)
-        self.number_line_left.place(relx = 0.01, rely = 0.01, relwidth = 0.10, height = 465)
+        self.number_line_left.place(relx = 0.01, rely = 0.01, relwidth = 0.10, height = 320)
         self.number_line_left.config(wrap = tk.WORD, font = ("Courier", 12))
         self.number_line_left.insert(END, f"1\n")
         self.number_line_left.config(state = "disabled")
 
         # Create a text widget for code input
         self.code_input = Text(self.left_frame, bg = "#E2E2E2", borderwidth = 0, padx = 5, pady = 5)
-        self.code_input.place(relx = 0.11, rely = 0.01, relwidth = 0.88, height = 465)
+        self.code_input.place(relx = 0.11, rely = 0.01, relwidth = 0.88, height = 320)
         self.code_input.config(wrap = tk.WORD, font = ("Courier", 12))
 
         # Create a text widget for output number line
@@ -81,6 +98,10 @@ class GUI:
         scrollbar.place(relx = 0.01, y = 450, relwidth = 0.98, height = 20)
         self.display.config(state = "disabled", width = self.display.winfo_screenwidth(), xscrollcommand = True, wrap = NONE, font = ("Courier", 10), spacing1 = 4)
 
+        # Syntax Errors Label
+        lexeme_label = Label(master = self.third_frame, text = "SYNTAX ERRORS", fg = "black", bg = "#E2E2E2", font = ("Inter", 10, "bold"))
+        lexeme_label.place(relx = 0.01, y = 5)
+        
         # Lexeme Label
         lexeme_label = Label(master = self.right_frame, text = "LEXEME", fg = "black", bg = "#E2E2E2", font = ("Inter", 10, "bold"))
         lexeme_label.place(relx = 0.21, y = 10)
@@ -101,7 +122,7 @@ class GUI:
             highlightthickness = 0, 
             command = self.open_file
         )
-        self.open_btn.place(relx = 0.07, rely = 1.26, anchor = 's', y = -150)
+        self.open_btn.place(relx = 0.07, rely = 1.35, anchor = 's', y = -150)
 
         # Save DSCODE file button
         original_save_image = Image.open(r'C:\GitHub\DSCode-Lexical-Analyzer\DSCode Lexical Analyzer\Images\save.png')
@@ -115,7 +136,7 @@ class GUI:
             highlightthickness = 0, 
             command = self.save_file
         )
-        self.save_btn.place(relx = 0.19, rely = 1.26, anchor = 's', y = -150)
+        self.save_btn.place(relx = 0.19, rely = 1.35, anchor = 's', y = -150)
 
         # Run Lexical Analyzer button
         original_run_image = Image.open(r'C:\GitHub\DSCode-Lexical-Analyzer\DSCode Lexical Analyzer\Images\run.png')
@@ -128,7 +149,7 @@ class GUI:
             highlightthickness = 0, 
             command = self.run_file
         )
-        self.run_btn.place(relx = 0.91, rely = 1.26, anchor = 's', y = -150)
+        self.run_btn.place(relx = 0.91, rely = 1.35, anchor = 's', y = -150)
         
         # Export Analysis button
         original_export_image = Image.open(r'C:\GitHub\DSCode-Lexical-Analyzer\DSCode Lexical Analyzer\Images\export.png')
@@ -213,6 +234,7 @@ class GUI:
     def run_file(self):
         code = self.code_input.get(1.0, END)
         self.lexemes, self.lexemes_display, self.tokens, self.number_line = lexical_analyzer(code)
+        self.parser_lines, self.parser_result = syntax_analyzer(self.tokens, self.number_line)
 
         # Update code input text widget
         self.display.config(state = "normal")
@@ -231,6 +253,15 @@ class GUI:
             self.number_line_right.insert(END, f"{self.number_line[i]}\n")
 
         self.number_line_right.config(state = "disabled")
+
+        #Update syntax errors text widget
+        self.syntax_errors.config(state = "normal")
+        self.syntax_errors.delete(1.0, END)
+
+        for item1, item2 in zip(self.parser_lines, self.parser_result):
+            self.syntax_errors.insert(END, f"{item1}\t{item2}\n")
+
+        self.syntax_errors.config(state = "disabled")
 
     # Export Analysis
     def export_file(self):
