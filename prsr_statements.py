84e4624f046bdf4c0a1bc2b350e2a290bf33f830
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-02-06 15:40:59 +0800

    Update prsr_statements.py

--- prsr_statements.py
+++ prsr_statements.py
@@ -1,21 +1,19 @@
-from prsr_arraypop import pop_first_element
+from prsr_otherfunctions import pop_first_element
 from prsr_errors import error_expected_after
-from prsr_assignments import parse_assignment
-from prsr_declaration import parse_declaration
-from prsr_controlflow import parse_controlflow
-from prsr_otherkeywords import parse_otherkeywords
-from prsr_expressions import parse_expression
 
-def parse_statement(number_line, tokens, lexemes, lines, result, Node):
+def parse_statements(number_line, tokens, lexemes, lines, result, Node):
     
     if tokens and tokens[0] == "LBRACE":
         popped_lbrace = pop_first_element(number_line, tokens, lexemes)
-
+        
         # Statements Nodes
 
         if tokens and tokens[0] == "RBRACE":
             pop_first_element(number_line, tokens, lexemes)
-            #return Node
+            node = Node("", []) # Add node
+            return node, lines, result
         
         else:
-            error_expected_after(lines, popped_lbrace[1], result, "')'", "'(' in expression")
+            node = Node("", [])
+            error_expected_after(lines, popped_lbrace[1], result, "'}'", "'{' in expression")
+            return node, lines, result
