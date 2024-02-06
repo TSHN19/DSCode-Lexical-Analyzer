dea481f134debe9ed2b32bbf871214f5f4bd4875
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-02-06 18:50:53 +0800

    Update prsr_controlflow.py

--- prsr_controlflow.py
+++ prsr_controlflow.py
@@ -2,6 +2,7 @@ from prsr_errors import error_missing_semicolon, error_invalid_syntax, error_exp
 from prsr_otherfunctions import pop_first_element
 from prsr_expressions import parse_boolexpression
 from prsr_statements import parse_statements
+from prsr_assignments import parse_assignment
 
 def parse_controlflow(number_line, tokens, lexemes, lines, result, Node):
     if lexemes[0] == "break":
@@ -11,42 +12,13 @@ def parse_controlflow(number_line, tokens, lexemes, lines, result, Node):
         return
     
     elif lexemes[0] == "do":
-        do_while_loop(number_line, tokens, lexemes, lines, result, Node)
+        return do_while_loop(number_line, tokens, lexemes, lines, result, Node)
     
     elif lexemes[0] == "else":
         return
      
     elif lexemes[0] == "for":
-        for_line_value = pop_first_element(number_line, tokens, lexemes)
-
-        if tokens[0] == "LPAREN":
-            lparen_line_value = pop_first_element(number_line, tokens, lexemes)
-            initialization = parse_statements(number_line, tokens, lexemes, lines, result, Node)
-
-            if tokens[0] == "SEMICOLON":
-                semicolon_line_value = pop_first_element(number_line, tokens, lexemes)
-                condition = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
-
-                if tokens[0] == "SEMICOLON":
-                    semicolon2_line_value = pop_first_element(number_line, tokens, lexemes)
-                    increment = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
-
-                    if tokens[0] == "RPAREN":
-                        pop_first_element(number_line, tokens, lexemes)
-                        body = parse_statements(number_line, tokens, lexemes, lines, result, Node)
-                        return Node("ForLoop", [initialization, condition, increment, body])
-                    else:
-                        lines.append(semicolon2_line_value)
-                        result.append("Expected ')' after '(' in expression")
-
-                else:
-                    error_missing_semicolon(lines, semicolon_line_value, result, "initialization")
-            else:
-                error_missing_semicolon(lines, lparen_line_value, result, "initialization")
-            
-        else:
-            lines.append(for_line_value)
-            result.append("Invalid for loop syntax")
+        return for_loop(number_line, tokens, lexemes, lines, result, Node)
     
     elif lexemes[0] == "goto":
         return
@@ -61,24 +33,31 @@ def parse_controlflow(number_line, tokens, lexemes, lines, result, Node):
         return
     
     elif lexemes[0] == "while":
-        number_line_value = pop_first_element(number_line, tokens, lexemes)
+        return while_loop(number_line, tokens, lexemes, lines, result, Node)
 
-        if tokens[0] == "LPAREN":
-            lparen_line_value = pop_first_element(number_line, tokens, lexemes)
-            condition = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
-            
-            if tokens[0] == "RPAREN":
-                pop_first_element(number_line, tokens, lexemes)
-                body = parse_statements(number_line, tokens, lexemes, lines, result, Node)
-                return Node("WhileLoop", [condition, body])
 
-            else:
-                lines.append(lparen_line_value)
-                result.append("Expected ')' after '(' in expression")
-                
+def while_condition(number_line, tokens, lexemes, lines, result, Node):
+    popped_while = pop_first_element(number_line, tokens, lexemes)
+
+    # If while is followed by a left parenthesis
+    if tokens[0] == "LPAREN":
+        popped_lparen = pop_first_element(number_line, tokens, lexemes)
+        condition = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
+
+        # Check if while loop has a condition and followed by a right parenthesis
+        if condition[0] and (tokens[0] == "RPAREN"):
+            popped_rparen = pop_first_element(number_line, tokens, lexemes)
+            node = Node("While", [condition[0]])
+            pop_first_element(number_line, tokens, lexemes)
+            return node, lines, result, popped_rparen[1]
+              
+        # Error if while condition not closed
         else:
-            lines.append(number_line_value)
-            result.append("Invalid for while loop syntax")
+            error_expected_after(lines, popped_lparen[1], result, ")", "while condition")
+
+    # Error if no left parenthesis after while keyword
+    else:
+        error_expected_after(lines, popped_while[1], result, "'('", " while keyword")
 
 def do_while_loop(number_line, tokens, lexemes, lines, result, Node):
     popped_do = pop_first_element(number_line, tokens, lexemes)
@@ -89,42 +68,96 @@ def do_while_loop(number_line, tokens, lexemes, lines, result, Node):
 
         # If statements is followed by the while keyword
         if lexemes[0] == "while":
-            popped_while = pop_first_element(number_line, tokens, lexemes)
+            condition = while_condition(number_line, tokens, lexemes, lines, result, Node)
 
-            # If while is followed by a left parenthesis
-            if tokens[0] == "LPAREN":
-                popped_lparen = pop_first_element(number_line, tokens, lexemes)
-                condition = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
-
-                # Check if while loop has a condition and followed by a right parenthesis
-                if condition[0] and (tokens[0] == "RPAREN"):
-                    popped_rparen = pop_first_element(number_line, tokens, lexemes)
+            # If identifier is followed by a semicolon
+            if tokens and tokens[0] == "SEMICOLON":
+                node = Node("Do-WhileLoop", [Node("Do", [statements[0]]), condition[0]])
+                pop_first_element(number_line, tokens, lexemes)
+                return node, lines, result
                     
-                    # If identifier is followed by a semicolon
-                    if tokens and tokens[0] == "SEMICOLON":
-                        node = Node("Do-While Loop", [Node("Do", [statements]), Node("While", [condition])])
-                        pop_first_element(number_line, tokens, lexemes)
-                        return node, lines, result
-                    
-                    # Error if declaration is missing semicolon
-                    else:
-                        error_missing_semicolon(lines, popped_rparen[1], result, "do-while loop")
-                
-                # Error if while condition not closed
-                else:
-                    error_expected_after(lines, popped_lparen[1], result, ")", "while condition")
-
-            # Error if no left parenthesis after while keyword
+            # Error if declaration is missing semicolon
             else:
-                error_expected_after(lines, popped_while[1], result, "'('", " while keyword")
+                error_missing_semicolon(lines, condition[3], result, "do-while loop")
         
         # Error if not followed by a while condition
         else:
             error_expected_after(lines, statements[1], result, "while", "'}' in the statement")
     
-    # Error if invalid syntax for do-while
+    # Error if invalid syntax for do-while loop
     else:
         error_invalid_syntax(lines, popped_do[1], result, "Do-While Loop")
     
     node = Node("", [])
     return node, lines, result
+
+def while_loop(number_line, tokens, lexemes, lines, result, Node):
+    condition = while_condition(number_line, tokens, lexemes, lines, result, Node)
+
+    # Check if while keyword is followed by a condition
+    if condition[0]:
+        statements = parse_statements(number_line, tokens, lexemes, lines, result, Node)
+        node = Node("WhileLoop", [condition[0], statements[0]])
+        pop_first_element(number_line, tokens, lexemes)
+        return node, lines, result
+
+    # Error if invalid syntax for for loop        
+    else:
+        error_invalid_syntax(lines, condition[3], result, "While Loop")
+
+def for_loop(number_line, tokens, lexemes, lines, result, Node):
+    popped_for = pop_first_element(number_line, tokens, lexemes)
+    
+    if tokens and (tokens[0] == "LPAREN"):
+        popped_lparen = pop_first_element(number_line, tokens, lexemes)
+        initialization = parse_assignment(number_line, tokens, lexemes, lines, result, Node)
+
+        if tokens and initialization[0]:
+            if tokens and (tokens[0] == "SEMICOLON"):
+                popped_semicolon = pop_first_element(number_line, tokens, lexemes)
+                condition = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
+
+                if tokens and condition[0]:
+                    if tokens and (tokens[0] == "SEMICOLON"):
+                        popped_semicolon = pop_first_element(number_line, tokens, lexemes)
+                        increment = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
+
+                        if tokens and increment[0]:
+                            if tokens and (tokens[0] == "RPAREN"):
+                                statements = parse_statements(number_line, tokens, lexemes, lines, result, Node)
+                                pop_first_element(number_line, tokens, lexemes)
+                                return Node("ForLoop", [initialization, condition, increment, statements])
+                            
+                            else:
+                                node = Node("", [])
+                                error_expected_after(lines, increment[1], result, "')'", "update")
+                                return node, lines, result
+                            
+                        else:
+                            node = Node("", [])
+                            error_expected_after(lines, popped_semicolon[1], result, "update", "';'")
+                            return node, lines, result
+
+                    else:
+                        node = Node("", [])
+                        error_missing_semicolon(lines, condition[1], result, "initialization")
+                        return node, lines, result
+                
+                else:
+                    node = Node("", [])
+                    error_expected_after(lines, popped_semicolon[1], result, "condition", "';'")
+                    return node, lines, result
+
+            else:
+                node = Node("", [])
+                error_missing_semicolon(lines, initialization[1], result, "initialization")
+        
+        else:
+            node = Node("", [])
+            error_expected_after(lines, popped_lparen[1], result, "initialization", "'('")
+            return node, lines, result
+    
+    else:
+        node = Node("", [])
+        error_invalid_syntax(lines, popped_for[1], result, "For Loop")
+        return node, lines, result
