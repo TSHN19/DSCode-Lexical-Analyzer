d3a48bb42652c8caa9bae79f3de52906e6a6b9d4
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-02-06 18:50:43 +0800

    Update prsr_assignments.py

--- prsr_assignments.py
+++ prsr_assignments.py
@@ -1,4 +1,4 @@
-from prsr_errors import error_missing_semicolon
+from prsr_errors import error_missing_semicolon, error_invalid_syntax
 from prsr_otherfunctions import pop_first_element
 from prsr_errors import error_expected_after
 from prsr_expressions import parse_boolexpression
@@ -15,18 +15,15 @@ def parse_assignment(number_line, tokens, lexemes, lines, result, Node):
     # If identifier is followed by an assignment operator
     if tokens and tokens[0] in assignment_operators:
         popped_operator = pop_first_element(number_line, tokens, lexemes)
-        expression = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
-    
-        # If expression is followed by semicolon
-        if tokens and tokens[0] == "SEMICOLON":
-            node = Node("Assignment", [popped_identifier[0], expression])
-            pop_first_element(number_line, tokens, lexemes)
+
+        if not tokens[0]:
+            node = Node("", [])
+            error_invalid_syntax(lines, popped_operator[1], result, "Assignment")
             return node, lines, result
         
-        # Error if assignment is missing semicolon
         else:
-            node = Node("", [])
-            error_missing_semicolon(lines, popped_operator[1], result, "assignment")
+            expression = parse_boolexpression(number_line, tokens, lexemes, lines, result, Node)
+            node = Node("Assignment", [popped_identifier[0], expression])
             return node, lines, result
     
     # Error if identifier is not followed by an assignment operator
