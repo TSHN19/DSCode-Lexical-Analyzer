84a7c1e29cd11582936534643faeceb0eb850037
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-02-06 15:37:55 +0800

    Delete prsr_arraypop.py

--- prsr_arraypop.py
+++
@@ -1,5 +0,0 @@
-def pop_first_element(number_line, tokens, lexemes):
-    tokens_value = tokens.pop(0)
-    lexemes.pop(0)
-    number_line_value = number_line.pop(0)
-    return tokens_value, number_line_value
