2491756d797672c5e3d3baf7e7433158a43b5c5f
TSHN19 <65933808+TSHN19@users.noreply.github.com>
2024-02-06 15:39:55 +0800

    Update prsr_errors.py

--- prsr_errors.py
+++ prsr_errors.py
@@ -1,3 +1,14 @@
+
+def error_invalid_syntax(lines, value, result, keyword):
+    lines.append(value)
+    result.append("Invalid " + keyword + " syntax")
+    return
+
+def error_unexpected_tokens(lines, value, result, token):
+    lines.append(value)
+    result.append(f"ERROR Unexpected token: {token}")
+    return
+
 # Error for missing semicolon
 def error_missing_semicolon(lines, value, result, after_token):
     lines.append(value)
